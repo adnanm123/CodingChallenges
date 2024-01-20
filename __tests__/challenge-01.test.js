ef3f731f18e4092a10f1a8b775afcb8ab727e3d0
Adnan Mohamud <adnanmohamud6@gmail.com>
2024-01-20 13:03:11 -0800

    completed code challenge 07

--- __tests__/challenge-01.test.js
+++ __tests__/challenge-01.test.js
@@ -1,4 +1,4 @@
-const { addOne, addExclamation, allUpperCase, speaker, greeting, addValues, addNumbers, createList} = require('../code-challenges/challenge-01');
+const { addOne, addExclamation, allUpperCase, speaker, greeting, addValues, addNumbers, createList, fizzbuzz} = require('../code-challenges/challenge-01');
 
 /* ------------------------------------------------------------------------------------------------
 TESTS
@@ -51,7 +51,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   const inputs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
   test('It should print out messages or numbers', () => {
