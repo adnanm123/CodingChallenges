4cc8fed30c1a7821a99fee895d41ec44b40b0915
Adnan Mohamud <adnanmohamud6@gmail.com>
2024-02-12 15:37:37 -0800

    completed challenge 2 question 4

--- code-challenges/challenge-02.js
+++ code-challenges/challenge-02.js
@@ -38,4 +38,20 @@ const addQuestion = (str) => {
   return str.map(s => s + '?');
 }
 
-module.exports = {raisedToTheThird, addOne, addQuestion};
+/*
+------------------------------------------------------------------------------------------------
+CHALLENGE 4
+
+Write a function named forLoopTwoToThe that, given an array of integers as input, iterates over the array and returns a new array. The returned array should contain the result of raising 2 to the power of the original input element.
+
+You may choose to complete this challenge using a for loop, for...in syntax, or for...of syntax.
+
+For example, twoToThe([1,2,3]) returns [2,4,8] because 2 ^ 1 = 2, 2 ^ 2 = 4, and 2 ^ 3 = 8.
+------------------------------------------------------------------------------------------------ */
+
+const forLoopTwoToThe = (int) => {
+  for(let i = 0; i <= int.length; i++)
+  return int.map( n => Math.pow(2, n));
+}
+
+module.exports = {raisedToTheThird, addOne, addQuestion, forLoopTwoToThe};
